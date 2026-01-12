29bfbaefb71707bec3aaec4bbf9922ea1a66adc6
tomas.izquierdo <tomas.izquierdo@globant.com>
2026-01-12 14:41:06 -0300

    FInalización del apartado 1

---
+++ String.Calculator.Tests/StringCalculator.cs
@@ -0,0 +1,42 @@
+﻿namespace String.Calculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(0, calc.add(""));
+
+        }
+
+        [Fact]
+        public void Add_SingleNumber_ReturnsSameNumber()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(20, calc.add("20"));
+
+        }
+
+        [Fact]
+        public void Add_SingleNumber_ReturnsSum()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(5001, calc.add("5000,1"));
+
+        }
+        [Fact]
+        public void Add_SingleNumber_TreatedAsZero()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(5, calc.add("5,qwerty"));
+        }
+        [Fact]
+        public void Add_MaxTwoNumbers_ThrowsException()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            var ex = Assert.Throws<Exception>(() => calc.add("1,2,3"));
+            Assert.Equal("Solo se permiten hasta 2 números",ex.Message);
+        }
+    }
+}
--- StringCalculatore-Core/StringCalculator.cs
+++ StringCalculatore-Core/StringCalculator.cs
@@ -1,10 +1,28 @@
-﻿namespace StringCalculatore.Core
+﻿namespace StringCalculator.Core
 {
     public class StringCalculator
     {
         public int add(string input)
         {
-            return 0;
+
+            if (string.IsNullOrWhiteSpace(input))
+                return 0;
+
+            string[] partes = input.Split(',');
+            if (partes.Length > 2)
+                throw new Exception("Solo se permiten hasta 2 números");
+            int suma = 0;
+            foreach (var parte in partes) 
+            {
+                if (int.TryParse(parte, out int numero))
+                {
+                    suma += numero;
+                } else {
+                    suma += 0;
+                }
+            
+            }
+            return suma;
         }
 
     }
