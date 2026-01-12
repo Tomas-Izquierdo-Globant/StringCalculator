135157c3d9c42f00b85faffb74133123f512e5e9
tomas.izquierdo <tomas.izquierdo@globant.com>
2026-01-12 17:27:14 -0300

    Se terminó el Apartado 5 y se añadió la restricción de que si es mayor a 1000 no sume y lo omita junto con el test.

---
+++ String.Calculator.Tests/StringCalculator.cs
@@ -0,0 +1,66 @@
+﻿namespace String.Calculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(0, calc.add(""));
+
+        }
+
+        [Fact]
+        public void Add_SingleNumber_ReturnsSameNumber()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(20, calc.add("20"));
+
+        }
+
+        [Fact]
+        public void Add_SingleNumber_ReturnsSum()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(999, calc.add("998,1"));
+
+        }
+        [Fact]
+        public void Add_SingleNumber_TreatedAsZero()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(5, calc.add("5,qwerty"));
+        }
+        [Fact]
+        public void Add_MultipleNumbers_ReturnsSum()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(78, calc.add("1,2,3,4,5,6,7,8,9,10,11,12"));
+
+        }
+        [Fact]
+        public void AddNewLineDelimeter_ReturnsSum()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(6, calc.add("1\n2,3"));
+
+        }
+        [Fact]
+        public void AddNegativeNumbers_ThrowsException()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            var ex = Assert.Throws<ArgumentException>(() => calc.add("2,-4,3,-5"));
+
+            Assert.Contains("-4", ex.Message);
+            Assert.Contains("-5", ex.Message);
+
+        }
+        [Fact]
+        public void Add_NumberGreaterThan1000_Ignored()
+        {
+            var calc = new StringCalculator.Core.StringCalculator();
+            Assert.Equal(8, calc.add("2,1001,6"));
+
+        }
+    }
+}
--- StringCalculatore-Core/StringCalculator.cs
+++ StringCalculatore-Core/StringCalculator.cs
@@ -1,10 +1,35 @@
-﻿namespace StringCalculatore.Core
+﻿namespace StringCalculator.Core
 {
     public class StringCalculator
     {
         public int add(string input)
         {
-            return 0;
+
+            if (string.IsNullOrWhiteSpace(input))
+                return 0;
+
+            string[] partes = input.Split(new char[] {',', '\n' });
+            int suma = 0;
+            List<int> negativos = new List<int>();
+            foreach (var parte in partes) 
+            {
+                if (int.TryParse(parte, out int numero))
+                {
+                    if (numero < 0)
+                    {
+                        negativos.Add(numero);
+                    }
+                    else if  (numero <= 1000)
+                        suma += numero;
+                }
+            
+            }
+
+            if (negativos.Count > 0)
+            {
+                throw new ArgumentException("números negativos no permitidos: " + string.Join(", ", negativos));
+            }
+            return suma;
         }
 
     }
